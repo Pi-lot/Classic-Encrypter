1ff4dc1c9ac0bf3ea1721089ab5f9b3f40fac26f
Bryce Tuton <brycey@tuton.org>
2019-09-10 20:35:56 +1000

    Started Tranposition cipher (didn't realise it was the wrong branch)

--- Classic Encryption/Classic Encryption/Transposition.cpp
+++ Classic Encryption/Classic Encryption/Transposition.cpp
@@ -1,1 +1,50 @@
 #include "Transposition.h"
+
+// Checks if the character is already in the key
+bool InKey(int num, int period, int key[]) {
+	bool contains = false;
+	for (int i = 0; i < period; i++)
+		if (key[i] == num) {
+			contains = true;
+			break;
+		}
+	return contains;
+}
+
+Transposition::MessageInfo Transposition::Encode(MessageInfo message) {
+	if (message.key == NULL) {
+		random_device rd;
+		if (message.period == NULL) {
+			do {
+				message.period = rd() % message.message.length();
+			} while (message.message.length() % message.period != 0);
+		}
+
+		message.key = new int[message.period];
+		for (int i = 0; i < message.period; i++)
+			message.key[i] = NULL;
+
+		for (int i = 0; i < message.period; i++) {
+			int num;
+			do {
+				num = rd() % message.period;
+			} while (InKey(num, message.period, message.key));
+		}
+	}
+	for (int i = 0; i < message.message.length(); i += message.period) {
+		char *temp = new char[message.period];
+		for (int j = 0; j < message.period; j++) {
+			temp[j] = message.message[i + j];
+		}
+		for (int j = 0; j < message.period; j++) {
+			message.message[i] = temp[message.key[j]];
+		}
+		delete[] temp;
+	}
+	delete[] message.key;
+	return message;
+}
+
+string Transposition::Decode(MessageInfo message) {
+	return string();
+}
--- Classic Encryption/Classic Encryption/Transposition.h
+++ Classic Encryption/Classic Encryption/Transposition.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <iostream>
+#include <random>
 
 using namespace std;
 
 class Transposition {
+public:
+	struct MessageInfo {
+		int *key;
+		int period;
+		string message;
+	};
+	MessageInfo Encode(MessageInfo message);
+	string Decode(MessageInfo message);
 };
 
